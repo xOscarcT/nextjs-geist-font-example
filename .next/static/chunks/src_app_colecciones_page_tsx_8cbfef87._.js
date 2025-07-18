99f969bbe042e5b168d79e8a672f31abc4489d82
xOscarcT <75442899+xOscarcT@users.noreply.github.com>
2025-07-18 15:11:50 -0500

    Add .next/static/chunks/src_app_colecciones_page_tsx_8cbfef87._.js 1752869510551

---
+++ .next/static/chunks/src_app_colecciones_page_tsx_8cbfef87._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b8300214._.js"
+],
+    source: "dynamic"
+});
