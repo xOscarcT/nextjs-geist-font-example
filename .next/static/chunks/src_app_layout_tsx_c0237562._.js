400964541bc8b341503e842339f0c00ada4eb6d2
xOscarcT <75442899+xOscarcT@users.noreply.github.com>
2025-07-18 15:12:12 -0500

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1752869532340

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_6c40afc4._.js",
+  "static/chunks/src_components_ui_Navbar_tsx_0e38d87e._.js"
+],
+    source: "dynamic"
+});
